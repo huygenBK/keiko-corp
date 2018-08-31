e209c6bc759a7add95817830239f9e89610612ed
huygenBK <baronk0401@gmail.com>
2018-08-31 15:42:07 +0800

    separate responsive css, add media attr, add before images

--- js/main.js
+++ js/main.js
@@ -18,24 +18,11 @@ $(document).ready(function(){
 	});
 
 
-
-
-
-
-
-
 	//Show & Hide menu on mobile
-	$('.burger_icon').click(function(){
-		$('header nav').toggleClass('show');
-		$('header .burger_icon').toggleClass('active');
-	});
-
-	
-
-
-
-
-
+$('.burger_icon').click(function(){
+  $('header nav').toggleClass('show');
+  $('header .burger_icon').toggleClass('active');
+});
 
 	//wow.js on scroll animations initialization
 	wow = new WOW(
